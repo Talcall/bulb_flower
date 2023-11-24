a38e33c42f4803cff6bf6bd68ec5bc80be4bf73a
Talcall <thomaswells2803@gmail.com>
2023-11-25 09:21:35 +1030

    Initial commit

---
+++ src/main/java/net/t4lcall/bulb_flower/BulbFlowerClient.java
@@ -0,0 +1,8 @@
+package net.t4lcall.bulb_flower;
+import net.fabricmc.api.ClientModInitializer;
+public class BulbFlowerClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
