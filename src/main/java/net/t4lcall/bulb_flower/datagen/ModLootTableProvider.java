84804976619bc569e63db6cadb68a1bee796bcb6
Forseir <151981867+Forseir@users.noreply.github.com>
2023-11-25 22:22:35 +1100

    LootTable updated
    Added processed blocks

--- src/main/java/net/t4lcall/bulb_flower/datagen/ModLootTableProvider.java
+++ src/main/java/net/t4lcall/bulb_flower/datagen/ModLootTableProvider.java
@@ -5,19 +5,28 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 import net.t4lcall.bulb_flower.block.ModBlocks;
 
 public class ModLootTableProvider extends FabricBlockLootTableProvider {
-    public ModLootTableProvider(FabricDataOutput dataOutput) {
+    protected ModLootTableProvider(FabricDataOutput dataOutput) {
         super(dataOutput);
     }
 
     @Override
     public void generate() {
         addDrop(ModBlocks.BULB_PLANKS);
+        addDrop(ModBlocks.BULB_ROOTLOG);
         addDrop(ModBlocks.BULB_ROOTS);
-        addDrop(ModBlocks.BULB_SAPLING);
-        addDrop(ModBlocks.BULB_PISTIL);
         addDrop(ModBlocks.BULB_ROOTLOG);
         addDrop(ModBlocks.BULB_ROOTWOOD);
-        addDrop(ModBlocks.STRIPPED_BULB_ROOTLOG);
-        addDrop(ModBlocks.STRIPPED_BULB_ROOTWOOD);
+        addDrop(ModBlocks.BULB_SAPLING);
+        addDrop(ModBlocks.BULB_STAIRS);
+        addDrop(ModBlocks.BULB_PISTIL);
+        addDrop(ModBlocks.BULB_BUTTON);
+        addDrop(ModBlocks.BULB_PRESSURE_PLATE);
+        addDrop(ModBlocks.BULB_TRAPDOOR);
+        addDrop(ModBlocks.BULB_FENCE_GATE);
+
+        addDrop(ModBlocks.BULB_DOOR, doorDrops(ModBlocks.BULB_DOOR));
+        addDrop(ModBlocks.BULB_SLAB, slabDrops(ModBlocks.BULB_SLAB));
+
+
     }
 }
