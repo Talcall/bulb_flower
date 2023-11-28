81ecc2bf109d98e3b81b48a179dfd87a960d7732
Talcall <thomaswells2803@gmail.com>
2023-11-28 15:12:20 +1030

    I just wanted to also make it so that you could till the roots off of end stone.

--- src/main/java/net/t4lcall/bulb_flower/block/RootedStoneBlock.java
+++ src/main/java/net/t4lcall/bulb_flower/block/RootedStoneBlock.java
@@ -3,6 +3,7 @@ package net.t4lcall.bulb_flower.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.SweetBerryBushBlock;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -15,6 +16,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 public class RootedStoneBlock extends Block {
     public RootedStoneBlock(Settings settings) {
@@ -34,5 +36,18 @@ public class RootedStoneBlock extends Block {
             RootedStoneBlock.dropStack(world, pos, new ItemStack(ModBlocks.ROOTED_END_STONE, 1));
         }
         super.onBreak(world, pos, this.getDefaultState(), player);
+    }
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!player.getStackInHand(hand).isIn(ItemTags.HOES)) {
+            return ActionResult.PASS;
+        }
+        if (player.getStackInHand(hand).isIn(ItemTags.HOES)) {
+            RootedStoneBlock.dropStack(world, pos, new ItemStack(ModBlocks.BULB_ROOTS, 1));
+            world.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.BLOCK_BEEHIVE_SHEAR, SoundCategory.BLOCKS, 1f, 1.75f);
+            world.setBlockState(pos, Blocks.END_STONE.getDefaultState());
+            return ActionResult.success(world.isClient);
+        }
+        return super.onUse(state, world, pos, player, hand, hit);
     }
 }
