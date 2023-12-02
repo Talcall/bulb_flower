4554e708c8f85d77c5b970625ce3245dd1e8708b
Talcall <thomaswells2803@gmail.com>
2023-12-02 18:45:07 +1030

    Add ability to right click on pistil with en empty hand.

--- src/main/java/net/t4lcall/bulb_flower/block/PistilBlock.java
+++ src/main/java/net/t4lcall/bulb_flower/block/PistilBlock.java
@@ -9,6 +9,9 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -54,6 +57,21 @@ public class PistilBlock extends FlowerBlock {
                 world.spawnEntity(areaEffectCloudEntity);
         }
         super.onBreak(world, pos, this.getDefaultState(), player);
+    }
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(player.getMainHandStack().isEmpty()) {
+            AreaEffectCloudEntity areaEffectCloudEntity = new AreaEffectCloudEntity(world, pos.getX() + 0.5f, pos.getY(), pos.getZ() + 0.5f);
+            areaEffectCloudEntity.setRadius(1.5f);
+            areaEffectCloudEntity.setRadiusOnUse(-0.5f);
+            areaEffectCloudEntity.setWaitTime(6);
+            areaEffectCloudEntity.setDuration(areaEffectCloudEntity.getDuration() / 2);
+            areaEffectCloudEntity.setRadiusGrowth(-areaEffectCloudEntity.getRadius() / (float) areaEffectCloudEntity.getDuration());
+            areaEffectCloudEntity.addEffect(new StatusEffectInstance(this.blockEffects));
+            world.spawnEntity(areaEffectCloudEntity);
+            return ActionResult.success(world.isClient);
+        }
+        return super.onUse(state, world, pos, player, hand, hit);
     }
     @Override
     public List<SuspiciousStewIngredient.StewEffect> getStewEffects() {
